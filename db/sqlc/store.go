526a2c7d9cd726ea303117def854183e1243f04c
nikit34 <permikov134@mail.ru>
2022-02-03 23:56:26 +0300

    Add Transfer

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -37,3 +37,27 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error ) error 
 	}
 	return tx.Commit()
 }
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID int64 `json:"to_account_id"`
+	Amount int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer Transfer `json:"transfer"`
+	FromAccount Account `json:"from_account"`
+	ToAccount Account `json:"to_account"`
+	FromEntry Entry `json:"from_entry"`
+	ToEntry Entry `json:"to_entry"`
+}
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		return nil
+	})
+
+	return result, err
+}
