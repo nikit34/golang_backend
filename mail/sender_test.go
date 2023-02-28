e534cce47a7db44684875721f7998fb8d543c290
Nikita Permyakov <nikita.permyakov@skyeng.ru>
2023-03-01 03:17:35 +0500

    Add email sender

---
+++ mail/sender_test.go
@@ -0,0 +1,26 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/nikit34/template_backend/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="https://nikit34.github.io/">nikit34</a></p>
+	`
+	to := []string{"permikov134@mail.ru"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
