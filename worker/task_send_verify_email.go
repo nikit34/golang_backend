e028b2c34b8c6e9b8eec162e75a09bde7cb4bb33
nikitapermyakov <n.permikov@uzum.com>
2023-04-03 03:39:29 +0400

    Add verify emails

--- worker/task_send_verify_email.go
+++ worker/task_send_verify_email.go
@@ -68,7 +68,7 @@ func (processor *RedisTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Cont
 	}
 
 	subject := "Welcome to Template Backend"
-	verifyUrl := fmt.Sprintf("http://template-backend.org?id=%d&secret_code=%s", verifyEmail.ID, verifyEmail.SecretCode)
+	verifyUrl := fmt.Sprintf("http://localhost:8080/v1/verify_email?email_id=%d&secret_code=%s", verifyEmail.ID, verifyEmail.SecretCode)
 	content := fmt.Sprintf(`Hello %s, <br/>
 	Thank you for registering with us!<br/>
 	Please <a href="%s">click here</a> to verify your email address.<br/>
