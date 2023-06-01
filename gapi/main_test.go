3eb730ab9d3765526e3cf207eee3ecb2e50e53a6
nikitapermyakov <n.permikov@uzum.com>
2023-06-01 10:28:20 +0300

    Add unit tests

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,13 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	db "github.com/nikit34/template_backend/db/sqlc"
+	"github.com/nikit34/template_backend/token"
 	"github.com/nikit34/template_backend/util"
 	"github.com/nikit34/template_backend/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 
@@ -21,4 +25,16 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
