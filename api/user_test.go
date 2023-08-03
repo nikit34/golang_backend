0b526b26dacdb90639bf58c1741ba3d96c869327
nikit34 <permikov134@mail.ru>
2023-08-04 02:29:35 +0300

    Apply to user tests

--- api/user_test.go
+++ api/user_test.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/lib/pq"
 	mockdb "github.com/nikit34/template_backend/db/mock"
 	db "github.com/nikit34/template_backend/db/sqlc"
 	"github.com/nikit34/template_backend/util"
@@ -91,7 +90,7 @@ func TestCreateUserAPI(t *testing.T) {
 				store.EXPECT().
 					CreateUser(gomock.Any(), gomock.Any()).
 					Times(1).
-					Return(db.User{}, &pq.Error{Code: "23505"})
+					Return(db.User{}, db.ErrUniqueViolation)
 			},
 			checkResponse: func(recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusForbidden, recorder.Code)
